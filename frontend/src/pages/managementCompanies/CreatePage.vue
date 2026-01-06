85e4abe72f0d94796fcc8aa699efb8b144abed4d
Ilya A. Shlyakhovoy <bulgarus@inbox.ru>
2026-01-06 23:16:46 +0300

    feat: add full CRUD for Management Companies with forms and tables

--- frontend/src/pages/managementCompanies/CreatePage.vue
+++ frontend/src/pages/managementCompanies/CreatePage.vue
@@ -1,17 +1,307 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md">
-      <h5 class="q-my-none q-mb-md">Create Management Company</h5>
-      <q-card>
-        <q-card-section>
-          <p class="text-grey-7">Management company creation form will be here</p>
-        </q-card-section>
-      </q-card>
+    <div class="q-pa-md" style="max-width: 1200px; margin: 0 auto;">
+      <!-- Header -->
+      <div class="row items-center q-mb-md">
+        <div class="col">
+          <h5 class="q-my-none">{{ $t('managementCompanies.create') }}</h5>
+        </div>
+        <div class="col-auto">
+          <q-btn
+            flat
+            icon="arrow_back"
+            :label="$t('common.back')"
+            @click="$router.back()"
+            no-caps
+          />
+        </div>
+      </div>
+
+      <!-- Form -->
+      <q-form @submit="onSubmit" class="q-gutter-md">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6 q-mb-md">{{ $t('managementCompanies.basicInfo') }}</div>
+            
+            <div class="row q-col-gutter-md">
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.name"
+                  :label="$t('common.name') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+              
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.registrationNumber"
+                  :label="$t('managementCompanies.registrationNumber') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12">
+                <q-input
+                  v-model="form.legalAddress"
+                  :label="$t('managementCompanies.legalAddress') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12 col-md-4">
+                <q-select
+                  v-model="form.country"
+                  :options="countryOptions"
+                  :label="$t('managementCompanies.country') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12 col-md-4">
+                <q-input
+                  v-model="form.city"
+                  :label="$t('managementCompanies.city') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12 col-md-4">
+                <q-input
+                  v-model="form.postalCode"
+                  :label="$t('managementCompanies.postalCode')"
+                  outlined
+                />
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+
+        <q-card>
+          <q-card-section>
+            <div class="text-h6 q-mb-md">{{ $t('managementCompanies.contactInfo') }}</div>
+            
+            <div class="row q-col-gutter-md">
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.email"
+                  :label="$t('common.email') + ' *'"
+                  type="email"
+                  outlined
+                  :rules="[
+                    val => !!val || $t('validation.required'),
+                    val => /.+@.+\..+/.test(val) || $t('validation.email')
+                  ]"
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.phone"
+                  :label="$t('common.phone') + ' *'"
+                  outlined
+                  mask="+# (###) ###-####"
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.directorName"
+                  :label="$t('managementCompanies.directorName') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.directorEmail"
+                  :label="$t('managementCompanies.directorEmail') + ' *'"
+                  type="email"
+                  outlined
+                  :rules="[
+                    val => !!val || $t('validation.required'),
+                    val => /.+@.+\..+/.test(val) || $t('validation.email')
+                  ]"
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.accountantName"
+                  :label="$t('managementCompanies.accountantName')"
+                  outlined
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.accountantEmail"
+                  :label="$t('managementCompanies.accountantEmail')"
+                  type="email"
+                  outlined
+                  :rules="[
+                    val => !val || /.+@.+\..+/.test(val) || $t('validation.email')
+                  ]"
+                />
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+
+        <q-card>
+          <q-card-section>
+            <div class="text-h6 q-mb-md">{{ $t('managementCompanies.bankingInfo') }}</div>
+            
+            <div class="row q-col-gutter-md">
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.bankName"
+                  :label="$t('managementCompanies.bankName')"
+                  outlined
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.bankAccount"
+                  :label="$t('managementCompanies.bankAccount')"
+                  outlined
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.swift"
+                  :label="$t('managementCompanies.swift')"
+                  outlined
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-input
+                  v-model="form.iban"
+                  :label="$t('managementCompanies.iban')"
+                  outlined
+                />
+              </div>
+
+              <div class="col-12 col-md-6">
+                <q-select
+                  v-model="form.currency"
+                  :options="currencyOptions"
+                  :label="$t('managementCompanies.currency') + ' *'"
+                  outlined
+                  :rules="[val => !!val || $t('validation.required')]"
+                />
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
+
+        <!-- Actions -->
+        <div class="row q-gutter-md justify-end">
+          <q-btn
+            :label="$t('common.cancel')"
+            outline
+            color="grey"
+            @click="$router.back()"
+            no-caps
+          />
+          <q-btn
+            type="submit"
+            :label="$t('common.create')"
+            color="primary"
+            :loading="loading"
+            no-caps
+            unelevated
+          />
+        </div>
+      </q-form>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-export default defineComponent({ name: 'CreateManagementCompanyPage' });
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useI18n } from 'vue-i18n';
+import { useQuasar } from 'quasar';
+import { managementCompaniesAPI } from '../../api';
+
+export default defineComponent({
+  name: 'CreateManagementCompanyPage',
+
+  setup() {
+    const router = useRouter();
+    const { t } = useI18n();
+    const $q = useQuasar();
+
+    const loading = ref(false);
+    const form = ref({
+      name: '',
+      registrationNumber: '',
+      legalAddress: '',
+      country: '',
+      city: '',
+      postalCode: '',
+      email: '',
+      phone: '',
+      directorName: '',
+      directorEmail: '',
+      accountantName: '',
+      accountantEmail: '',
+      bankName: '',
+      bankAccount: '',
+      swift: '',
+      iban: '',
+      currency: 'EUR'
+    });
+
+    const countryOptions = [
+      'Russia', 'Bulgaria', 'Poland', 'Germany', 'France', 'Spain', 'Italy'
+    ];
+
+    const currencyOptions = [
+      'EUR', 'USD', 'RUB', 'BGN', 'PLN'
+    ];
+
+    const onSubmit = async () => {
+      loading.value = true;
+
+      try {
+        await managementCompaniesAPI.create(form.value);
+        
+        $q.notify({
+          message: t('managementCompanies.createSuccess'),
+          color: 'positive',
+          icon: 'check'
+        });
+        
+        router.push('/management-companies');
+      } catch (error) {
+        $q.notify({
+          message: error.message || t('common.error'),
+          color: 'negative',
+          icon: 'error'
+        });
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    return {
+      loading,
+      form,
+      countryOptions,
+      currencyOptions,
+      onSubmit
+    };
+  }
+});
 </script>
--- frontend/src/pages/managementCompanies/ListPage.vue
+++ frontend/src/pages/managementCompanies/ListPage.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page padding>
     <div class="q-pa-md">
+      <!-- Header -->
       <div class="row items-center q-mb-md">
         <div class="col">
           <h5 class="q-my-none">{{ $t('nav.managementCompanies') }}</h5>
+          <p class="text-grey-7 q-mb-none">{{ $t('managementCompanies.subtitle') }}</p>
         </div>
         <div class="col-auto">
           <q-btn
@@ -12,37 +14,121 @@
             :label="$t('common.create')"
             @click="$router.push('/management-companies/create')"
             no-caps
+            unelevated
           />
         </div>
       </div>
 
-      <q-card>
+      <!-- Filters -->
+      <q-card class="q-mb-md">
         <q-card-section>
-          <q-input
-            v-model="search"
-            :label="$t('common.search')"
-            outlined
-            dense
-            clearable
-          >
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
+          <div class="row q-col-gutter-md">
+            <div class="col-12 col-md-6">
+              <q-input
+                v-model="filters.search"
+                :label="$t('common.search')"
+                outlined
+                dense
+                clearable
+                @update:model-value="onSearch"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="search" />
+                </template>
+              </q-input>
+            </div>
+            <div class="col-12 col-md-3">
+              <q-select
+                v-model="filters.status"
+                :options="statusOptions"
+                :label="$t('common.status')"
+                outlined
+                dense
+                emit-value
+                map-options
+                clearable
+                @update:model-value="fetchCompanies"
+              />
+            </div>
+            <div class="col-12 col-md-3">
+              <q-select
+                v-model="filters.country"
+                :options="countryOptions"
+                :label="$t('managementCompanies.country')"
+                outlined
+                dense
+                clearable
+                @update:model-value="fetchCompanies"
+              />
+            </div>
+          </div>
         </q-card-section>
+      </q-card>
 
+      <!-- Table -->
+      <q-card>
         <q-table
           :rows="companies"
           :columns="columns"
           row-key="id"
           :loading="loading"
+          :pagination="pagination"
+          @request="onRequest"
           flat
           bordered
         >
+          <!-- Status badge -->
+          <template v-slot:body-cell-status="props">
+            <q-td :props="props">
+              <q-badge
+                :color="props.row.status === 'active' ? 'positive' : 'negative'"
+                :label="$t(`common.${props.row.status}`)"
+              />
+            </q-td>
+          </template>
+
+          <!-- Complex count -->
+          <template v-slot:body-cell-complexCount="props">
+            <q-td :props="props">
+              <q-chip color="primary" text-color="white" size="sm">
+                {{ props.row.complexCount || 0 }}
+              </q-chip>
+            </q-td>
+          </template>
+
+          <!-- Actions -->
           <template v-slot:body-cell-actions="props">
             <q-td :props="props">
-              <q-btn flat round dense icon="visibility" color="primary" @click="viewCompany(props.row.id)" />
-              <q-btn flat round dense icon="edit" color="secondary" @click="editCompany(props.row.id)" />
+              <q-btn
+                flat
+                round
+                dense
+                icon="visibility"
+                color="primary"
+                @click="viewCompany(props.row.id)"
+              >
+                <q-tooltip>{{ $t('common.view') }}</q-tooltip>
+              </q-btn>
+              <q-btn
+                flat
+                round
+                dense
+                icon="edit"
+                color="secondary"
+                @click="editCompany(props.row.id)"
+              >
+                <q-tooltip>{{ $t('common.edit') }}</q-tooltip>
+              </q-btn>
+              <q-btn
+                flat
+                round
+                dense
+                icon="delete"
+                color="negative"
+                @click="confirmDelete(props.row)"
+              >
+                <q-tooltip>{{ $t('common.delete') }}</q-tooltip>
+              </q-btn>
             </q-td>
           </template>
         </q-table>
@@ -52,9 +138,11 @@
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
+import { defineComponent, ref, onMounted, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
+import { useQuasar } from 'quasar';
+import { managementCompaniesAPI } from '../../api';
 
 export default defineComponent({
   name: 'ManagementCompaniesListPage',
@@ -62,25 +150,137 @@ export default defineComponent({
   setup() {
     const router = useRouter();
     const { t } = useI18n();
-    const search = ref('');
+    const $q = useQuasar();
+
     const loading = ref(false);
     const companies = ref([]);
+    const filters = ref({
+      search: '',
+      status: null,
+      country: null
+    });
 
-    const columns = [
-      { name: 'name', label: t('common.name'), field: 'name', align: 'left', sortable: true },
-      { name: 'email', label: t('common.email'), field: 'email', align: 'left' },
-      { name: 'phone', label: t('common.phone'), field: 'phone', align: 'left' },
-      { name: 'complexCount', label: 'Complexes', field: 'complexCount', align: 'center' },
-      { name: 'status', label: t('common.status'), field: 'status', align: 'center' },
-      { name: 'actions', label: t('common.actions'), field: 'actions', align: 'center' }
-    ];
+    const pagination = ref({
+      sortBy: 'name',
+      descending: false,
+      page: 1,
+      rowsPerPage: 10,
+      rowsNumber: 0
+    });
 
-    const fetchCompanies = async () => {
+    const columns = computed(() => [
+      {
+        name: 'name',
+        label: t('common.name'),
+        field: 'name',
+        align: 'left',
+        sortable: true
+      },
+      {
+        name: 'registrationNumber',
+        label: t('managementCompanies.registrationNumber'),
+        field: 'registrationNumber',
+        align: 'left'
+      },
+      {
+        name: 'country',
+        label: t('managementCompanies.country'),
+        field: 'country',
+        align: 'left',
+        sortable: true
+      },
+      {
+        name: 'email',
+        label: t('common.email'),
+        field: 'email',
+        align: 'left'
+      },
+      {
+        name: 'phone',
+        label: t('common.phone'),
+        field: 'phone',
+        align: 'left'
+      },
+      {
+        name: 'complexCount',
+        label: t('managementCompanies.complexes'),
+        field: 'complexCount',
+        align: 'center',
+        sortable: true
+      },
+      {
+        name: 'status',
+        label: t('common.status'),
+        field: 'status',
+        align: 'center',
+        sortable: true
+      },
+      {
+        name: 'actions',
+        label: t('common.actions'),
+        field: 'actions',
+        align: 'center'
+      }
+    ]);
+
+    const statusOptions = computed(() => [
+      { label: t('common.active'), value: 'active' },
+      { label: t('common.inactive'), value: 'inactive' }
+    ]);
+
+    const countryOptions = ref([
+      'Russia', 'Bulgaria', 'Poland', 'Germany', 'France'
+    ]);
+
+    const fetchCompanies = async (props = {}) => {
       loading.value = true;
-      // TODO: Implement API call
-      await new Promise(resolve => setTimeout(resolve, 500));
-      companies.value = [];
-      loading.value = false;
+
+      try {
+        const { page, rowsPerPage, sortBy, descending } = props.pagination || pagination.value;
+        
+        const params = {
+          page,
+          limit: rowsPerPage,
+          sortBy,
+          order: descending ? 'desc' : 'asc',
+          search: filters.value.search,
+          status: filters.value.status,
+          country: filters.value.country
+        };
+
+        const response = await managementCompaniesAPI.getAll(params);
+        
+        companies.value = response.data.data || [];
+        pagination.value.page = response.data.page || 1;
+        pagination.value.rowsPerPage = response.data.limit || 10;
+        pagination.value.rowsNumber = response.data.total || 0;
+        
+        if (props.pagination) {
+          pagination.value.sortBy = sortBy;
+          pagination.value.descending = descending;
+        }
+      } catch (error) {
+        $q.notify({
+          message: error.message || t('common.error'),
+          color: 'negative',
+          icon: 'error'
+        });
+      } finally {
+        loading.value = false;
+      }
+    };
+
+    const onRequest = (props) => {
+      fetchCompanies(props);
+    };
+
+    let searchTimeout;
+    const onSearch = () => {
+      clearTimeout(searchTimeout);
+      searchTimeout = setTimeout(() => {
+        pagination.value.page = 1;
+        fetchCompanies();
+      }, 500);
     };
 
     const viewCompany = (id) => {
@@ -91,17 +291,54 @@ export default defineComponent({
       router.push(`/management-companies/${id}/edit`);
     };
 
+    const confirmDelete = (company) => {
+      $q.dialog({
+        title: t('common.confirm'),
+        message: t('managementCompanies.deleteConfirm', { name: company.name }),
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        await deleteCompany(company.id);
+      });
+    };
+
+    const deleteCompany = async (id) => {
+      try {
+        await managementCompaniesAPI.delete(id);
+        
+        $q.notify({
+          message: t('managementCompanies.deleteSuccess'),
+          color: 'positive',
+          icon: 'check'
+        });
+        
+        await fetchCompanies();
+      } catch (error) {
+        $q.notify({
+          message: error.message || t('common.error'),
+          color: 'negative',
+          icon: 'error'
+        });
+      }
+    };
+
     onMounted(() => {
       fetchCompanies();
     });
 
     return {
-      search,
       loading,
       companies,
+      filters,
+      pagination,
       columns,
+      statusOptions,
+      countryOptions,
+      onRequest,
+      onSearch,
       viewCompany,
-      editCompany
+      editCompany,
+      confirmDelete
     };
   }
 });
